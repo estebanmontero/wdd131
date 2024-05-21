ea0a96ad1e88914a3dc1c777c5f87d362e4e57c0
estebanmontero <estebanmontero@hotmail.es>
2024-05-20 19:56:46 -0500

    new version commit javascript

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,7 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav ul');
 
-
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
